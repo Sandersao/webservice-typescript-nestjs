4af132f841b7d7ac3ce2cab7a0b9858063a0355c
Sanderson Rocha <sanduba753@gmail.com>
2025-06-16 10:28:18 -0300

    Swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigAdapter } from './business/adapter/config.adapter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const configAdapter: ConfigAdapter = new ConfigAdapter()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Product menegment')
+    .setDescription('This api helps on meneging products, note, does not menege the inventory')
+    .addTag('product')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+
   await app.listen(configAdapter.getApplication().port, () => {
     if (configAdapter.getGlobal().env == 'DEVELOP') {
       console.debug(`Application ruinning on door ${configAdapter.getApplication().port}`)
