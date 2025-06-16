7e1c310f09896ef686c9abbb28d2bb21ab18375e
Sanderson Rocha <sanduba753@gmail.com>
2025-06-16 08:01:26 -0300

    Implementacao do resgate do env

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+  await app.listen(process.env.PORT ?? 3000, () => {
+    if ((process.env.ENVIRONMENT ?? 'PRODUCTION') == 'DEVELOP') {
+      console.debug(`Application ruinning on door ${process.env.PORT}`)
+    }
+  })
 }
 
-bootstrap();
+config()
+bootstrap()
