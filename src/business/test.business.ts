c218aa60d43bdea465c96e20401b9d20e71d6922
Sanderson Rocha <sanduba753@gmail.com>
2025-06-22 12:59:08 -0300

    Implementing of error handler structure

--- src/business/test.business.ts
+++ src/business/test.business.ts
@@ -4,6 +4,7 @@ import { TestListRequest } from "src/controller/request/test-list.request";
 import { TestInsertRequest } from "src/controller/request/test-insert.request";
 import { TestUpdateRequest } from "src/controller/request/test-update.request";
 import { TestDeleteRequest } from "src/controller/request/test-delete.request";
+import { NoContentException } from "src/exeption/no-content.exception";
 
 @Injectable()
 export class TestBusiness {
@@ -12,11 +13,16 @@ export class TestBusiness {
     ) { }
 
     public async select(request: TestListRequest) {
-        return this.repository.select(request)
+        const testList = await this.repository.select(request)
+
+        if(testList.length == 0){
+            throw new NoContentException('No test recovered for the informed filters')
+        }
 
         /** @todo Trow this exception as a all right error */
         /** It must be throun when there is no itens on the item */
         // 'No test recovered for the filters informed'
+        return testList
     }
 
     public async insert(request: TestInsertRequest) {
---
+++ src/exeption/no-content.exception.ts
@@ -0,0 +1,7 @@
+import { HttpException, HttpStatus } from "@nestjs/common";
+
+export class NoContentException extends HttpException {
+    constructor(message: string){
+        super(message, HttpStatus.NO_CONTENT)
+    }
+}
